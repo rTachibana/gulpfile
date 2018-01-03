35e98c2a9abbd56b6c1f55b7be84a70ba8d7b328
tachibana-4 <elements.lezard+git@gmail.com>
2018-01-03 15:36:19 +0900

    scssのコンパイル処理を追加、画像圧縮をjpg,gif,svgにも適応できるよう改修

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,30 +1,33 @@
 // Minimum config
-var fs        = require('fs');
-var path      = require('path');
+const fs        = require('fs');
+const path      = require('path');
 
-var gulp      = require('gulp');
-var plumber   = require('gulp-plumber');
-var rename    = require('gulp-rename');
-var scss      = require('gulp-sass');
-var pcss      = require('gulp-postcss');
-var maps      = require('gulp-sourcemaps');
-var imgMin    = require('gulp-imagemin');
-var imgMinP   = require('imagemin-pngquant');
-var imgMinJ   = require('imagemin-mozjpeg');
+const gulp      = require('gulp');
+const plumber   = require('gulp-plumber');
+const rename    = require('gulp-rename');
+const sass      = require('gulp-sass');
+const stylus    = require('gulp-stylus');
+const pcss      = require('gulp-postcss');
+const csso      = require('gulp-csso');
+const maps      = require('gulp-sourcemaps');
+const imgMin    = require('gulp-imagemin');
+const imgMinP   = require('imagemin-pngquant');
+const imgMinJ   = require('imagemin-mozjpeg');
 // and more packages
 // ----
+// css-mqpacker
 // typescript-require
 // autoprefixer
-var getFolders = (dir) => {
+const getFolders = (dir) => {
     return fs.readdirSync(dir)
         .filter((file) => {
             return fs.statSync(path.join(dir, file)).isDirectory();
         });
 }
 
-var date = require('date-utils');
-var dt = new Date();
-var ts = dt.toFormat("YYYYMMDDHH24MI");
+const date = require('date-utils');
+const dt = new Date();
+const ts = dt.toFormat("YYYYMMDDHH24MI");
 
 interface Path {
     src_css: string;
@@ -33,43 +36,54 @@ interface Path {
     dst_img: string;
 }
 // setting paths
-var Path = {
+const Path = {
     src_css: './source/scss/',
-    src_img: './source/img/',
+    src_img: './source/img/**/*.*',
     dst_css: './publish/css/',
     dst_img: './publish/img/',
     bk_img: './_bk/'
 }
 
-
 // plefix optimize
-var browser = [
+const browser = [
     "last 1 safari versions",
     'ie >=  11',
     'android >= 5'
 ]
-var quality =  '80-90';
-
+const quality = 80;
 gulp.task('t',() => {
     console.log(ts);
-})
+});
+
+gulp.task('css', function () {
+    return gulp.src( Path.src_css + '**/**' )
+        .pipe(maps.init())
+        .pipe(sass( {outputStyle: 'expanded' }))
+        .pipe(pcss([
+            require('css-mqpacker')(),
+            require('autoprefixer')({
+                browsers: browser
+            }),
+        ]))
+        .pipe(maps.write('./'))
+        .pipe(gulp.dest( Path.dst_css ))
+});
 
 gulp.task('img', () => {
-    gulp.src( './source/img/' + '*.png' )
-        .pipe(gulp.dest(Path.bk_img + ts))
+    gulp.src( Path.src_img )
+        .pipe(gulp.dest( Path.bk_img + ts ))
         .pipe(imgMin([
             imgMinP({
                 quality: quality,
                 speed: 1,
-                floyd: 0
+                verbose: false
             }),
             imgMinJ({
                 quality: quality,
-                speed: 1,
                 progressive: true
             }),
             imgMin.svgo(),
             imgMin.optipng()
         ]))
-        .pipe(gulp.dest(Path.dst_img));
+        .pipe(gulp.dest( Path.dst_img ));
 });
